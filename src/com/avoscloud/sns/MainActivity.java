4d649bdaa58e82ceba8bdbc8226cfede697a8429
xiaobo <jacob.xiaobo@gmail.com>
2015-04-02 17:04:07 +0800

    add SSO auth demo dependency by maven use ButterKnife

--- src/com/avoscloud/sns/MainActivity.java
+++ src/com/avoscloud/sns/MainActivity.java
@@ -12,89 +12,141 @@ import com.avos.sns.SNSType;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends Activity {
   private SNSType type = null;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    View sinaAuth = findViewById(R.id.loginWithSina);
-    View qqAuth = findViewById(R.id.loginWithQQ);
-
-    sinaAuth.setOnClickListener(new View.OnClickListener() {
-
-      @Override
-      public void onClick(View v) {
-        try {
-          type = SNSType.AVOSCloudSNSSinaWeibo;
-          SNS.setupPlatform(SNSType.AVOSCloudSNSSinaWeibo,
-              "https://leancloud.cn/1.1/sns/goto/70uczc8byq0bgchy");
-          /*
-           * 这个url是在app下对应的组件中的SNS页面，在保存相应平台后的appId和app SecretId之后生成的登录URL
-           * 
-           * 同时，你需要将生成的回调URL，填写到相应平台的回调地址中间去
-           */
-
-          SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSSinaWeibo,
-              new SNSCallback() {
-
-                @Override
-                public void done(SNSBase base, SNSException error) {
-                  if (error == null) {
-                    SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
-                      @Override
-                      public void done(AVUser user, AVException error) {
-
-                      }
-                    });
-                  }
-                }
-              });
-        } catch (AVException e) {
-          e.printStackTrace();
-        }
-      }
-    });
-
-    qqAuth.setOnClickListener(new View.OnClickListener() {
-
-      @Override
-      public void onClick(View v) {
-        try {
-          type = SNSType.AVOSCloudSNSQQ;
-          SNS.setupPlatform(SNSType.AVOSCloudSNSQQ,
-              "https://leancloud.cn/1.1/sns/goto/kspiihdtpzn4186e");
-          /*
-           * 这个url是在app下对应的组件中的SNS页面，在保存相应平台后的appId和app SecretId之后生成的登录URL
-           * 
-           * 同时，你需要将生成的回调URL，填写到相应平台的回调地址中间去
-           */
-
-          SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSQQ,
-              new SNSCallback() {
-
-                @Override
-                public void done(SNSBase base, SNSException error) {
-                  if (error == null) {
-                    SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
-                      @Override
-                      public void done(AVUser user, AVException error) {
-
-                      }
-                    });
-                  }
-                }
-              });
-        } catch (AVException e) {
-          e.printStackTrace();
-        }
-      }
-    });
-
+    ButterKnife.inject(this);
   }
+
+  @OnClick(R.id.login_with_sina)
+  void loginWithSina() {
+    try {
+      type = SNSType.AVOSCloudSNSSinaWeibo;
+      SNS.setupPlatform(SNSType.AVOSCloudSNSSinaWeibo,
+          "https://leancloud.cn/1.1/sns/goto/70uczc8byq0bgchy");
+      /*
+       * 这个url是在app下对应的组件中的SNS页面，在保存相应平台后的appId和app SecretId之后生成的登录URL
+       * 同时，你需要将生成的回调URL，填写到相应平台的回调地址中间去
+       */
+
+      SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSSinaWeibo,
+          new SNSCallback() {
+
+            @Override
+            public void done(SNSBase base, SNSException error) {
+              if (error == null) {
+                SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
+                  @Override
+                  public void done(AVUser user, AVException error) {
+
+                  }
+                });
+              }
+            }
+          });
+    } catch (AVException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @OnClick(R.id.login_with_qq)
+  void loginWithQq() {
+    try {
+      type = SNSType.AVOSCloudSNSQQ;
+      SNS.setupPlatform(SNSType.AVOSCloudSNSQQ,
+          "https://leancloud.cn/1.1/sns/goto/kspiihdtpzn4186e");
+      /*
+       * 这个url是在app下对应的组件中的SNS页面，在保存相应平台后的appId和app SecretId之后生成的登录URL
+       * 同时，你需要将生成的回调URL，填写到相应平台的回调地址中间去
+       */
+
+      SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSQQ,
+          new SNSCallback() {
+
+            @Override
+            public void done(SNSBase base, SNSException error) {
+              if (error == null) {
+                SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
+                  @Override
+                  public void done(AVUser user, AVException error) {
+
+                  }
+                });
+              }
+            }
+          });
+    } catch (AVException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @OnClick(R.id.login_with_sina_sso)
+  void loginWithSinaSso() {
+    try {
+      type = SNSType.AVOSCloudSNSSinaWeibo;
+      SNS.setupPlatform(this, SNSType.AVOSCloudSNSSinaWeibo, "YOUR_SINA_WEIBO_APP_ID",
+          "YOUR_SINA_WEIBO_APP_KEY",
+          "YOUR_SINA_WEIBO_CALLBACK_URL");
+
+      SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSSinaWeibo,
+          new SNSCallback() {
+
+            @Override
+            public void done(SNSBase base, SNSException error) {
+              if (error == null) {
+                SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
+                  @Override
+                  public void done(AVUser user, AVException error) {
+
+                  }
+                });
+              }
+            }
+          });
+    } catch (AVException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @OnClick(R.id.login_with_qq_sso)
+  void loginWithQqSso() {
+    try {
+      type = SNSType.AVOSCloudSNSQQ;
+      SNS.setupPlatform(this, SNSType.AVOSCloudSNSQQ, "YOUR_QQ_APP_ID",
+          "YOUR_QQ_APP_KEY",
+          "YOUR_QQ_CALLBACK_URL");
+
+      SNS.loginWithCallback(MainActivity.this, SNSType.AVOSCloudSNSQQ,
+          new SNSCallback() {
+
+            @Override
+            public void done(SNSBase base, SNSException error) {
+              if (error == null) {
+                SNS.loginWithAuthData(base.userInfo(), new LogInCallback<AVUser>() {
+                  @Override
+                  public void done(AVUser user, AVException error) {
+
+                  }
+                });
+              }
+            }
+          });
+    } catch (AVException e) {
+      e.printStackTrace();
+    }
+  }
+
+
 
   /**
    * 需要复制onActivityResult的代码
